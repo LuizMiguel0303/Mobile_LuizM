e54801a0fc10fb3b84520c8cdfbf2647c9fac7fb
LuizMiguel0303 <luiz.elzer.leite@escola.pr.gov.br>
2026-07-22 14:43:21 -0400

    Create ex2.js

---
+++ aula1/ex2.js
@@ -0,0 +1,9 @@
+const somar = (a, b) => a + b;
+const subtrair = (a, b) => a - b;
+const multiplicar = (a, b) => a * b;
+const dividir = (a, b) => a / b;
+
+console.log("Soma (10 + 5):", somar(10, 5));
+console.log("Subtração (10 - 5):", subtrair(10, 5));
+console.log("Multiplicação (10 * 5):", multiplicar(10, 5));
+console.log("Divisão (10 / 5):", dividir(10, 5));
