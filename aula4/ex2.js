7cdecb974b9a1d88ed6e23a2adcbd4367aff1187
LuizMiguel0303 <luiz.elzer.leite@escola.pr.gov.br>
2026-07-22 15:02:10 -0400

    Create ex2.js

---
+++ aula4/ex2.js
@@ -0,0 +1,14 @@
+const numeros = [2, 4, 6, 8, 10];
+
+const dobro = numeros.map(numero => numero * 2);
+
+console.log("Original:", numeros);
+console.log("Novo:", dobro);
+Ex3:
+
+const precos = [50, 80, 100, 150];
+
+const descontos = precos.map(preco => preco * 0.9);
+
+console.log("Preços originais:", precos);
+console.log("Com desconto:", descontos);
