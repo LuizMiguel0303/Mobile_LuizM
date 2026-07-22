5fbd085e77869efeca391f26b885a3388a3eef51
LuizMiguel0303 <luiz.elzer.leite@escola.pr.gov.br>
2026-07-22 15:02:32 -0400

    Create ex4.js

---
+++ aula4/ex4.js
@@ -0,0 +1,12 @@
+const idades = [12, 17, 18, 21, 15, 30];
+
+const maiores = idades.filter(idade => idade >= 18);
+
+console.log(maiores);
+Ex5:
+
+const nomes = ["João", "Maria", "Carlos", "Ana"];
+
+const maiusculos = nomes.map(nome => nome.toUpperCase());
+
+console.log(maiusculos);
