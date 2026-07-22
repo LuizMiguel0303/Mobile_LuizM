7112cecf4f1de3094d3e9dcc5eebd5c83a999e66
LuizMiguel0303 <luiz.elzer.leite@escola.pr.gov.br>
2026-07-22 14:55:47 -0400

    Create ex5.js

---
+++ aula2/ex5.js
@@ -0,0 +1,13 @@
+function triploDeclaracao(x) {
+    return x * 3;
+}
+
+const triploExpressao = function(x) {
+    return x * 3;
+};
+
+const triploArrow = x => x * 3;
+
+console.log(triploDeclaracao(5));
+console.log(triploExpressao(5));
+console.log(triploArrow(5));
